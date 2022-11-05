34d40f9f4b2e4ee25e601e1d70efc9ad3c532acf
Benson Zhang <BZhang705@hotmail.com>
2022-11-06 03:05:19 +1100

    Add GameManager and Modifiers. Also add basic test prefab, scripts and other resources.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// README:
+// Access the instance and its properties through GameManager.Instance
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager Instance {get {return _instance;}}
+    public List<Modifier> modifiers;
+
+
+    private void Awake(){
+        if(_instance != null && _instance != this){
+            Destroy(this.gameObject);
+        }else{
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    void Start(){
+        modifiers = new List<Modifier>();
+
+        ShotDelayModifier foo = new ShotDelayModifier();
+        foo.Init(FindObjectOfType<Weapon>());
+        modifiers.Add(foo);
+
+        BiggerBulletsModifier bar = new BiggerBulletsModifier();
+        bar.Init(FindObjectOfType<Weapon>());
+        modifiers.Add(bar);
+    }
+
+    void Update(){
+        
+    }
+}
