217e0e09d4dc766fabe2fea84cfe1730f2f49daa
usrlss <josh.white@ctemplar.com>
2022-11-06 22:55:41 +1100

    testing GIT

---
+++ Assets/Scripts/GitTest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this was a triumph
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
